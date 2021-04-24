9544652ada94b963b9ff95994eeabf6d0be9d206
Ankita <63940647+JaZz-9@users.noreply.github.com>
2021-04-24 23:52:08 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,7 +30,7 @@ function Navbar() {
                     </li>
                     <li className="nav-item">
                         <NavLink to="/projects" exact activeClassName="active">
-                            My Works
+                            Portfolio
                         </NavLink>
                     </li>
                 </ul>
