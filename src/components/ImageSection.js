0425b0c65525a465e4d4615c9d849877594a3ee2
Ankita <63940647+JaZz-9@users.noreply.github.com>
2021-04-24 23:50:27 +0530

    Update ImageSection.js

--- src/components/ImageSection.js
+++ src/components/ImageSection.js
@@ -12,7 +12,7 @@ function ImageSection() {
                 <p className="about-text">
                     a pre-final year student at IIT ISM Dhanbad, India, pursuing Integrated Masters of Technology in Applied Geophysics.
                     I am a Machine Learning enthusiast and an Open Source Contributor who loves to work on diverse projects having a blend of software development and ML. 
-                    I believe in creating great web and android applications that have a strong machine learning foundation from enhancing productivity to creating solid solutions. The belief that motivates me to code and contribute to the community is that my solutions would directly affect the society in some way and might help in making it a better place to live.
+                    I believe in creating great web and android applications that have a strong machine learning foundation from enhancing productivity to creating solid solutions. The belief that motivates me to code and contribute to the community is that my solutions would directly affect society in some way and might help in making it a better place to live.
                 </p>
                 <button className="btn">Download Cv</button>
             </div>
