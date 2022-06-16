ba96b84f29ad73e6b0e890bce6445c787239b3df
ankitajaiswal00 <ankitajaiswal334455@gmail.com>
2022-06-16 09:54:36 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -31,7 +31,6 @@ function App() {
               <Route path="/" exact>
                 <>
                 <Home />
-                 
                 </>
               </Route>
               <Route path="/about" exact>
